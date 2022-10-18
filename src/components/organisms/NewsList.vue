ce156f7f9a9821338b825b3bd96c6c761148e406
denis <denisaliev7@gmail.com>
2022-10-18 13:54:05 +0300

    newslist

--- src/components/organisms/NewsList.vue
+++ src/components/organisms/NewsList.vue
@@ -3,9 +3,17 @@
     <section class="mb-32 text-gray-800">
       <h2 class="text-3xl font-bold mb-12 text-center">Последние новости</h2>
 
-      <div class="grid lg:grid-cols-4 gap-6">
+      <div class="flex space-x-2 justify-center mb-10">
+        <button @click="getNews" type="button" class="inline-block px-6 py-2.5 bg-blue-600
+        text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700
+        hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+        active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
+          Обновить
+        </button>
+      </div>
 
-        <div v-for="(newsItem, index) in news" :key="index"
+      <div class="grid lg:grid-cols-4 gap-6">
+        <div v-for="(newsItem, index) in visibleNews" :key="index"
              class="relative overflow-hidden bg-no-repeat bg-cover shadow-lg rounded-lg"
              style="background-position: 50%;" data-mdb-ripple="true" data-mdb-ripple-color="light"
         >
@@ -26,20 +34,36 @@
             </div>
           </router-link>
         </div>
-
       </div>
-
     </section>
   </div>
 </template>
 
 <script setup>
 import api from '@/api';
-import { ref } from 'vue';
+import { ref, onMounted, computed } from 'vue';
 
-const news = ref(null);
+const news = ref([]);
+const countVisibleNews = ref(8);
+const visibleNews = computed(() => news.value.slice(0, countVisibleNews.value));
 
-api.getNews().then((res) => { news.value = res; });
-api.getDetailedNew(1).then((res) => { console.log(res); });
+const getNews = () => {
+  api.getNews().then((res) => { news.value = res; });
+};
 
+onMounted(() => {
+  getNews();
+
+  window.onscroll = () => {
+    const height = document.body.offsetHeight;
+    const screenHeight = window.innerHeight;
+    const scrolled = window.scrollY;
+    const threshold = height - screenHeight / 10;
+    const position = scrolled + screenHeight;
+
+    if (position >= threshold) {
+      countVisibleNews.value += 4;
+    }
+  };
+});
 </script>
